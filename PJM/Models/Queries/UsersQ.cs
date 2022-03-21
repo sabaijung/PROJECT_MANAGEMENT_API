b0945af3417c2aba73ff20e69bbfd0a208adefca
sabaijung <ardsri.s@gmail.com>
2022-03-21 09:46:26 +0700

    ปรับปรุงโค้ด เพื่อแบ่งเพจข้อมูล

--- PJM/Models/Queries/UsersQ.cs
+++ PJM/Models/Queries/UsersQ.cs
@@ -12,7 +12,41 @@ namespace PJM.Models.Queries
         private readonly PROJECT_MANAGEMENTContext context = new PROJECT_MANAGEMENTContext();
 
 
-        public async Task<object> GetUsers()
+        public async Task<object> GetUsers(int pageSize = 10, int currentPage = 1, string search = "")
+        {
+            var query =  context.Users.AsQueryable();
+            if (!string.IsNullOrEmpty(search))
+            {
+                query = query.Where(a => a.Name.Contains(search));
+            }
+
+            int count = query.Count();
+            var item = query.Skip((currentPage - 1) * pageSize).Take(pageSize).ToList();
+            var result = (from a in item
+                          select new
+                          {
+                              a.Name,
+                              a.Lastname
+
+                          });
+
+            return new
+            {
+                StatusCode = count == 0 ? "000" : "001",
+                Message = count == 0 ? "ไม่พบข้อมูล" : "พบข้อมูล",
+                Pagin = new
+                {
+                    totlalPage = (int)Math.Ceiling((double)count / pageSize),
+                    totalRow = count,
+                    currentPage,
+                    pageSize
+                },
+                Data = result
+            };
+            
+        }
+
+        public async Task<object> GetUsers1()
         {
             var value = await context.Users.Where(a => a.Isused == "1").Select(a => new
             {
