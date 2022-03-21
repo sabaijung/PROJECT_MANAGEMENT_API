815e724f0b1ada1b150f7fe31d7a75dc16d4fdff
sabaijung <ardsri.s@gmail.com>
2022-03-21 10:44:39 +0700

    เพิ่มไฟล์โมดูล project

---
+++ PJM/Controllers/ProjectController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PJM.Models.Queries;
+using System;
+using System.Threading.Tasks;
+
+namespace PJM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : Controller
+    {
+        private readonly ProjectQ projectQ = new ProjectQ();
+        [HttpGet("GetProjects")]
+        public async Task<IActionResult> GetProjects(int pageSize = 10, int currentPage = 1, string search = "")
+        {
+            try
+            {
+                return Ok(await projectQ.GetProject(pageSize, currentPage, search));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { StatusCode = 400, Message = e.Message });
+            }
+        }
+    }
+}
---
+++ PJM/Models/Queries/ProjectQ.cs
@@ -0,0 +1,48 @@
+﻿using PJM.Models.Data;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PJM.Models.Queries
+{
+    public class ProjectQ
+    {
+        private readonly PROJECT_MANAGEMENTContext context = new PROJECT_MANAGEMENTContext();
+
+        public async Task<object> GetProject(int pageSize = 10, int currentPage = 1, string search = "")
+        {
+            var query = context.Projects.AsQueryable();
+            if (!string.IsNullOrEmpty(search))
+            {
+                query = query.Where(a => a.ProjectName.Contains(search));
+            }
+
+            int count = query.Count();
+            var item = query.Skip((currentPage - 1) * pageSize).Take(pageSize).ToList();
+            var result = (from a in item
+                          select new
+                          {
+                              a.Code,
+                              a.ProjectName,
+                              a.ProjectStatus,
+                              a.DateStart,
+                              a.DateEnd
+                          });
+
+            return new
+            {
+                StatusCode = count == 0 ? "000" : "001",
+                Message = count == 0 ? "ไม่พบข้อมูล" : "พบข้อมูล",
+                Pagin = new
+                {
+                    totlalPage = (int)Math.Ceiling((double)count / pageSize),
+                    totalRow = count,
+                    currentPage,
+                    pageSize
+                },
+                Data = result
+            };
+
+        }
+    }
+}
