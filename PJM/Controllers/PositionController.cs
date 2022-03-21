39960fa348d083b10cf73c4d2765930e59a1deb2
sabaijung <ardsri.s@gmail.com>
2022-03-21 10:34:42 +0700

    เพิ่มไฟล์

---
+++ PJM/Controllers/PositionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PJM.Models.Queries;
+using System;
+using System.Threading.Tasks;
+
+namespace PJM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PositionController : Controller
+    {
+        private readonly PositionQ positionQ = new PositionQ();
+        [HttpGet("GetPosition")]
+        public async Task<IActionResult> GetPosition(int deptCode)
+        {
+            try
+            {
+                return Ok(await positionQ.GetAllPosition(deptCode));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { StatusCode = 400, Message = e.Message });
+            }
+        }
+    }
+}
---
+++ PJM/Models/Queries/PositionQ.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PJM.Models.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PJM.Models.Queries
+{
+    public class PositionQ
+    {
+        private readonly PROJECT_MANAGEMENTContext context = new PROJECT_MANAGEMENTContext();
+        public async Task<object> GetAllPosition(int deptCode)
+        {
+            var value = await context.Positions.Where(p => p.DepartmentCode == deptCode &&  p.Isused == "1").Select(pos => new
+            {
+                pos.PositionCode,
+                pos.PositionName
+            }).ToListAsync();
+
+            return value;
+
+         }
+
+    }
+}
