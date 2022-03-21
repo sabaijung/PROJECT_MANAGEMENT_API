b09763fbcb2233b77849341310553b9d512ad907
sabaijung <ardsri.s@gmail.com>
2022-03-21 09:58:31 +0700

    เพิ่มไฟล์ department

---
+++ PJM/Controllers/DepartmentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PJM.Models.Queries;
+using System;
+using System.Threading.Tasks;
+
+namespace PJM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : Controller
+    {
+        private readonly DepartmentQ departmentQ = new DepartmentQ();
+        [HttpGet("GetAllDeparment")]
+        public async Task<IActionResult> GetUsers()
+        {
+            try
+            {
+                return Ok(await departmentQ.GetAllDepartment());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { StatusCode = 400, Message = e.Message });
+            }
+        }
+    }
+}
---
+++ PJM/Models/Queries/DepartmentQ.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PJM.Models.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PJM.Models.Queries
+{
+    public class DepartmentQ
+    {
+        private readonly PROJECT_MANAGEMENTContext context = new PROJECT_MANAGEMENTContext();
+        public async Task<object> GetAllDepartment()
+        {
+            var value = await context.Departments.Where(a => a.Isused == "1").Select(d => new
+            {
+                d.DepartmentCode,
+                d.DepartmentName
+            }).ToListAsync();
+
+            return value;
+
+         }
+
+    }
+}
