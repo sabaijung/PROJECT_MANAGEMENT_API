b1886275cf2eabe10fec999c6976f99bc44eca87
sabaijung <ardsri.s@gmail.com>
2022-04-11 11:40:43 +0700

    เพิ่มไฟล์

---
+++ PJM/Controllers/AuthenticationController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PJM.Models.Queries;
+using PJM.Models.Request;
+using System;
+using System.Threading.Tasks;
+
+namespace PJM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : Controller
+    {
+        private readonly AuthenticationQ _authenticationQ = new AuthenticationQ();
+        [HttpPost("Authen")]
+        public async Task<IActionResult> Login([FromBody] AuthenticationReq login)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var url = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
+                    var result = await _authenticationQ.Login(login, url);
+                    return StatusCode(200, result);
+                }
+                return BadRequest();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { StatusCode = 400, Message = e.Message });
+            }
+        }
+    }
+}
