d59ad963c85cf8500b4ede39f2593795a7d151bc
sabaijung <ardsri.s@gmail.com>
2022-04-11 11:30:49 +0700

    เพิ่มโค้ด JWT

--- PJM/Models/Queries/AuthenticationQ.cs
+++ PJM/Models/Queries/AuthenticationQ.cs
@@ -1,7 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using PJM.Models.Data;
 using PJM.Models.Request;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace PJM.Models.Queries
@@ -10,6 +16,27 @@ namespace PJM.Models.Queries
     {
         private readonly PROJECT_MANAGEMENTContext context = new PROJECT_MANAGEMENTContext();
 
+        public string GenerateAccessToken(string Code, string userType, int minute = 60)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("PJMHelpSecretKey"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new(JwtRegisteredClaimNames.Sub, Code));
+            claims.Add(new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+            claims.Add(new(ClaimTypes.Role, userType));
+
+            var token = new JwtSecurityToken(
+                  "https://localhost:44318",
+                  "https://localhost:44318",
+                  notBefore: DateTime.Now,
+                  expires: DateTime.Now.AddMinutes(minute),
+                  claims: claims,
+                  signingCredentials: credentials
+              );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
         public async Task<object> Login(AuthenticationReq authen, string url)
         {
             var user = await context.Users.Where(u => u.Username == authen.Username 
